e8c0a2cfedfb026f702275aac81be8b9dc7ee99c
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-04-05 10:41:02 -0700

    Update player.h

--- player.h
+++ player.h
@@ -1,9 +1,53 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+
+#include "cpputils/graphics/image.h"
+
 class Player {
  public:
- 
+  Player() {}
+  Player(int x, int y, int kWidth, int kHeight)
+        : x_(x), y_(y), kWidth_(kWidth), kHeight_(kHeight) {}
+  int GetX() { return x_; }
+  int GetY() { return y_; }
+  int GetWidth() { return kWidth_; }
+  int GetHeight() { return kHeight_; }
+  void SetX(int x) {
+      x_ = x;
+  }
+  void SetY(int y) {
+      y_ = y;
+  }
+  void Draw(graphics::Image& image);
+  bool IntersectWith(Opponent& opponent);
  private:
- 
+  int x_;
+  int y_;
+  int const kWidth_;
+  int const kHeight_;
+};
+
+class PlayerProjectile {
+ public:
+  PlayerProjectile() {}
+  PlayerProjectile(int x, int y, int kWidth, int kHeight)
+        : x_(x), y_(y), kWidth_(kWidth), kHeight_(kHeight) {}
+  int GetX() { return x_; }
+  int GetY() { return y_; }
+  int GetWidth() { return kWidth_; }
+  int GetHeight() { return kHeight_; }
+  void SetX(int x) {
+      x_ = x;
+  }
+  void SetY(int y) {
+      y_ = y;
+  }
+  void Draw(graphics::Image& image);
+  bool IntersectWith(Opponent& opponent);
+ private:
+  int x_;
+  int y_;
+  int const kWidth_;
+  int const kHeight_;
 };
 #endif
