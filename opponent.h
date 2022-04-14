42f46814636b12b3e8bb6c1127245ceccc2e6520
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-04-14 13:48:25 -0700

    Update opponent.h

--- opponent.h
+++ opponent.h
@@ -5,31 +5,22 @@
 
 class Opponent {
  public:
-  Opponent() : Opponent(0, 0){}
+  Opponent() : Opponent(0, 0) {}
   Opponent(int x, int y) : x_(x), y_(y) {}
-  int GetX() { return x_; }
-  int GetY() { return y_; }
+  int GetX() const { return x_; }
+  int GetY() const { return y_; }
   int GetWidth() { return kWidth_; }
   int GetHeight() { return kHeight_; }
-  void SetX(int x) {
-      x_ = x;
-  }
-  void SetY(int y) {
-      y_ = y;
-  }
-    
+  void SetX(int x) { x_ = x; }
+  void SetY(int y) { y_ = y; }
+
   void Draw(graphics::Image& image);
+
  private:
   int x_;
   int y_;
-  int const kWidth_ = 500;
-  int const kHeight_ = 500;
-
-  void PadPoints(std::vector<int> &points, int pad_x, int pad_y);
-
-  const graphics::Color kRed(255, 0 , 0);
-  const graphics::Color kOrange(255, 165 , 0);
-  const graphics::Color kYellow(255,  255, 0);
+  int const kWidth_ = 50;
+  int const kHeight_ = 50;
 };
 
 class OpponentProjectile {
@@ -40,25 +31,15 @@ class OpponentProjectile {
   int GetY() { return y_; }
   int GetWidth() { return kWidth_; }
   int GetHeight() { return kHeight_; }
-  void SetX(int x) {
-      x_ = x;
-  }
-  void SetY(int y) {
-      y_ = y;
-  }
-  void Draw(graphics::Image &image);
-  
+  void SetX(int x) { x_ = x; }
+  void SetY(int y) { y_ = y; }
+  void Draw(graphics::Image& image);
+
  private:
   int x_;
   int y_;
   int const kWidth_ = 5;
   int const kHeight_ = 5;
-  void PadPoints(std::vector<int> &points, int pad_x, int pad_y);
-
-  const graphics::Color kRed(255, 0 , 0);
-  const graphics::Color kOrange(255, 165 , 0);
-  const graphics::Color kYellow(255,  255, 0);
-  
 };
 void DrawOp();
 #endif
