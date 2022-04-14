487976e19074a9bcf567b456e296210085197789
LouisOporto <89407369+LouisOporto@users.noreply.github.com>
2022-04-14 13:46:59 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,17 +1,17 @@
 #include <iostream>
 
-#include "player.h"
 #include "opponent.h"
+#include "player.h"
 
 #include "cpputils/graphics/image.h"
 
 int main() {
-
   graphics::Image game_screen(800, 600);
   Player player(400, 500);
   PlayerProjectile pprojectile(400, 445);
   Opponent opponent(400, 100);
   OpponentProjectile oprojectile(400, 155);
+
   player.Draw(game_screen);
   pprojectile.Draw(game_screen);
   opponent.Draw(game_screen);
@@ -20,5 +20,6 @@ int main() {
   pprojectile.IntersectsWith(opponent);
 
   game_screen.ShowUntilClosed();
-return 0;
+
+  return 0;
 }
